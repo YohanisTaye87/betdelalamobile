1617fac54f6f92a9868c0d0f008b853518de1491
Yohanis Taye <tayeyohanis8@gmail.com>
2023-05-25 21:59:13 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:betdelalamobile/providers/houses.dart';
-import 'package:betdelalamobile/providers/products_screen.dart';
+//import 'package:betdelalamobile/providers/houses.dart';
+//import 'package:betdelalamobile/providers/products_screen.dart';
+import 'package:betdelalamobile/widget/product_grid.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'screens/detail_screen.dart';
-
-
+//import 'screens/detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,19 +12,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(value: Houses()),
+      providers: const [
+        //   ChangeNotifierProvider.value(value: Houses()),
       ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo)
+              .copyWith(secondary: Colors.deepOrange),
         ),
-        home: ProductScreen(),
-        routes: {
-          DetailScreen.routename: ((context) => DetailScreen()),
+        home: const ProductGrid(),
+        routes: const {
+          // DetailScreen.routename: ((context) => DetailScreen()),
         },
       ),
     );
